f93d229a73e1cdd1b0d4030c3c91bddf18271d55
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2021-09-26 06:11:50 +0100

    cc

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,4 +1,22 @@
-export default function Category() {
-  return <h1>This Is Sparta</h1>
+import { category, getAllCategories } from "../../lib/api"
+
+export default function Category({ categories, category }) {
+  return <h1>asdasd</h1>
 }
-export function getStaticProps() {}
+export async function getStaticProps({ params }) {
+  const categories = await getAllCategories()
+  const cate = await category(params.slug)
+  console.log("cate", cate)
+  return {
+    props: { categories: categories, category: cate },
+  }
+}
+
+export async function getStaticPaths() {
+  const cate = await getAllCategories()
+
+  return {
+    paths: cate.categories.edges.map(({ node }) => `${node.uri}`) || [],
+    fallback: false,
+  }
+}
