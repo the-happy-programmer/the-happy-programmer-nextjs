c8675881c0537e8bac97f7376f0232df0ab51339
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2021-10-17 23:17:15 +0100

    cc

--- components/PostBody.js
+++ components/PostBody.js
@@ -1,11 +1,16 @@
 import styles from "../styles/post.module.css"
-
+import { renderToStaticMarkup } from "react-dom/server"
+import { Parser } from "html-to-react"
 export default function Postbody({ content }) {
+  const e = new Parser()
+  const ReactElement = e.parse(content)
+  const dolores = renderToStaticMarkup(ReactElement)
+
   return (
     <div className='bg-gray-100 border-t border-b border-gray-200 dark:border-gray-600  dark:bg-gray-800'>
       <div
         className={styles.content}
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{ __html: dolores }}
       />
     </div>
   )
