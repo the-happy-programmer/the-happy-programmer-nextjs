ae34a9e8bfa339bbfce571015c2ae87f217f373f
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2021-12-31 19:38:05 +0000

    cc

--- components/Happybutton.js
+++ components/Happybutton.js
@@ -2,10 +2,11 @@ export default function HappyButton({ children, href, xl }) {
   return (
     <a
       href={href}
-      className={`hover:bg-gray-50 hover:text-gray-900 
+      className={`hover:bg-inherit hover:text-gray-900 
       ${xl ? 'text-base' : 'text-xs'}  
       ${xl ? 'py-3 px-10' : 'py-2 px-3'}
-        inline-block  border border-gray-900 dark:border-gray-50 bg-gray-900 text-gray-50 rounded-md focus:outline-none dark:bg-gray-50 dark:text-gray-900 dark:hover:border-gray-50 dark:hover:bg-gray-900 dark:hover:text-gray-50 cursor-pointer`}
+      ${xl ? 'border-2' : 'border'}
+        inline-block border-gray-900 dark:border-gray-50 bg-gray-900 text-gray-50 rounded-md focus:outline-none dark:bg-gray-50 dark:text-gray-900 dark:hover:border-gray-50 dark:hover:bg-gray-900 dark:hover:text-gray-50 cursor-pointer`}
     >
       {children}
     </a>
--- components/SubHero.js
+++ components/SubHero.js
@@ -5,7 +5,7 @@ export default function SubHero({ subhero }) {
     <div className='bg-gray-100 dark:bg-gray-800 border-t border-b border-gray-200 dark:border-gray-700'>
       <div className='container  flex gap-x-4 flex-row '>
         {subhero.map(([icon, title, subtitle]) => (
-          <div className='flex flex-col items-center py-20'>
+          <div className='flex flex-col items-center py-20' key={icon}>
             <div className='dark:bg-gray-700 bg-gray-200 p-4 rounded-full flex items-center justify-center w-24 h-24'>
               <SvgtoReact
                 class='fill-current dark:text-gray-100 stroke-current'
