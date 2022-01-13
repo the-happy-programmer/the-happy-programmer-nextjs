781524d6ef2ec2db79ff84d62da0666c29ca2749
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-01-13 20:41:43 +0000

    carousel 95%

---
+++ components/SubHero.js
@@ -0,0 +1,27 @@
+import SvgtoReact from './Svgtoreact'
+
+export default function SubHero({ subhero }) {
+  return (2
+    <div className='bg-gray-100 dark:bg-gray-800 border-t border-b border-gray-200 dark:border-gray-700'>
+      <div className='container  flex gap-x-4 flex-row '>
+        {subhero.map(([icon, title, subtitle]) => (
+          <div className='flex flex-col items-center py-20' key={icon}>
+            <div className='dark:bg-gray-700 bg-gray-200 p-4 rounded-full flex items-center justify-center w-24 h-24'>
+              <SvgtoReact
+                class='fill-current dark:text-gray-100 stroke-current'
+                height={35}
+                name={icon}
+              />
+            </div>
+            <div className='text-3xl font-bold dark:text-gray-100 py-8'>
+              {title}
+            </div>
+            <div className='dark:text-gray-300 text-gray-600 text-center text-lg mx-10'>
+              {subtitle}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- components/home/Carousel.js
+++ components/home/Carousel.js
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import SvgtoReact from '../Svgtoreact';
 
 export default function Carousel() {
+  const heroicons = ['heromobile', 'heroweb', 'herowatch', 'heroserver'];
+
+  const [svghero, setsvghero] = useState(0);
+  const setSvg = (svgcounter) => {
+    setsvghero(svgcounter);
+    console.log(svghero);
+  };
+
   return (
     <div>
-      <SvgtoReact height={500} width={400} name='heromobile' />
-      <div className='flex flex-row p-2 justify-center'>
-        <div className='h-2 w-2 rounded-full bg-gray-300 dark:bg-gray-700' />
-        <div className='h-2 w-2 rounded-full bg-gray-300 dark:bg-gray-700' />
-        <div className='h-2 w-2 rounded-full bg-gray-300 dark:bg-gray-700' />
+      {heroicons.map((e, index) => (
+        <SvgtoReact
+          height={500}
+          width={400}
+          className={svghero !== index && 'hidden'}
+          name={e}
+        />
+      ))}
+      <div className='flex flex-row p-2 justify-center '>
+        {heroicons.map((e, index) => (
+          <div
+            onClick={(e) => setSvg(index)}
+            className={`h-2 w-2 rounded-full mx-1 cursor-pointer shadow-2xl ${
+              index === svghero ? 'bg-gray-700' : 'bg-gray-200'
+            } ${index === svghero ? 'dark:bg-gray-200' : 'dark:bg-gray-400'}`}
+          />
+        ))}
       </div>
     </div>
   );
