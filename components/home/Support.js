2be291f82b6eeab1ce245c84267a1d5abc68e475
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-01-18 23:57:47 +0000

    support almost done 80%

--- components/home/Support.js
+++ components/home/Support.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import SvgtoReact from '../Svgtoreact';
 
 export default function Support() {
+  const [currentsupport, setcurrentsupport] = useState(0);
   const prices = ['£2,5', '£4', '£8'];
-  const benefits = ['Early access', 'Patron-only updates', 'Chat community'];
+  const benefits = [
+    ['Early access', 'Patron-only updates', 'Chat community'],
+    ['Early access', 'Patron-only updates', 'Chat community', 'Chat community'],
+    [
+      'Early access',
+      'Patron-only updates',
+      'Chat community',
+      'Chat community',
+      'Chat community',
+    ],
+  ];
   return (
     <div className='border-b dark:border-gray-700 text-gray-900 dark:text-gray-300'>
       <div className='container px-4 py-10'>
@@ -15,11 +27,15 @@ export default function Support() {
         <div className=''>
           <p>Choose Your Plan</p>
           <div className='flex justify-center gap-x-20'>
-            <div className='rounded-lg border-gray-700'>
-              {prices.map((price) => (
+            <div className='rounded-lg border-gray-700 flex flex-col gap-y-10'>
+              {prices.map((price, index) => (
                 <div
-                  className='rounded-md border px-6 py-2 dark:border-gray-700'
+                  className={`${
+                    index === currentsupport &&
+                    'dark:bg-gray-50 bg-gray-900 dark:text-gray-900 text-gray-50'
+                  } rounded-md border px-6 py-2 dark:border-gray-700`}
                   key={price}
+                  onClick={(e) => setcurrentsupport(index)}
                 >
                   <div className='flex flex-row gap-x-20 items-center'>
                     <div>
@@ -34,7 +50,14 @@ export default function Support() {
                 </div>
               ))}
             </div>
-            <div className=' border rounded-lg border-gray-700'>b</div>
+            <div className=' border rounded-lg border-gray-200 dark:border-gray-700 flex flex-col justify-center px-5'>
+              {benefits[currentsupport].map((e) => (
+                <div className='flex flex-row gap-x-5 m-5'>
+                  <SvgtoReact name='tick' />
+                  <p>{e}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
