904cccdd738e3ca75fbc48203df7206d99a0c4fe
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-02-13 22:59:17 +0000

    cc

--- components/home/SubHero.js
+++ components/home/SubHero.js
@@ -3,9 +3,9 @@ import SvgtoReact from '../Svgtoreact'
 export default function SubHero({ subhero }) {
   return (
     <div className="border-t border-b border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800">
-      <div className="container  flex flex-col gap-x-4 sm:flex-col md:flex-row xl:flex-row ">
+      <div className="container flex flex-col items-center justify-between px-3 sm:flex-col md:flex-row xl:flex-row">
         {subhero.map(([icon, title, subtitle]) => (
-          <div className="flex flex-col items-center py-20" key={icon}>
+          <div className="flex w-60 flex-col items-center py-20" key={icon}>
             <div className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-200 dark:bg-gray-700">
               <SvgtoReact
                 class="fill-current stroke-current text-gray-800  dark:text-gray-100"
@@ -16,7 +16,7 @@ export default function SubHero({ subhero }) {
             <div className="py-8 text-3xl font-bold dark:text-gray-100">
               {title}
             </div>
-            <div className="mx-10 text-center text-lg text-gray-600 dark:text-gray-300">
+            <div className="text-center text-lg text-gray-600 dark:text-gray-300">
               {subtitle}
             </div>
           </div>
