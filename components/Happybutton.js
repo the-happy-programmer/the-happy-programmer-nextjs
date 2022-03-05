e87e466257d8fd2b4cd5b1107e2549b0a1bffcb8
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-03-05 23:56:30 +0000

    cc

--- components/Happybutton.js
+++ components/Happybutton.js
@@ -6,7 +6,7 @@ export default function HappyButton({ children, href, xl }) {
       ${xl ? 'text-base' : 'text-xs'}
       ${xl ? 'py-3 px-10' : 'py-2 px-3'}
       ${xl ? 'border-2' : 'border'}
-        focus:outline-none inline-block cursor-pointer rounded-md border-gray-900 bg-gray-900 text-gray-50 shadow-lg dark:border-gray-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:border-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-50`}
+        focus:outline-none inline-block cursor-pointer rounded-md border-gray-900 bg-gray-900 text-gray-50 shadow-lg dark:border-gray-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:border-gray-50 dark:hover:bg-gray-200 `}
     >
       {children}
     </a>
