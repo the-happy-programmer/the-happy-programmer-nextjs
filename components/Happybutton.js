d9dd0ac2e8e693410ba48516582cc5fdf3482d0e
MyNameIsBond <hajdini@uni.coventry.ac.uk>
2022-03-14 03:58:58 +0000

    button changed

--- components/Happybutton.js
+++ components/Happybutton.js
@@ -2,11 +2,11 @@ export default function HappyButton({ children, href, xl }) {
   return (
     <a
       href={href}
-      className={`hover:bg-opacity-80
+      className={`leading-loose tracking-wide hover:bg-opacity-80
       ${xl ? 'text-base' : 'text-xs'}
-      ${xl ? 'py-3 px-10' : 'py-2 px-3'}
+      ${xl ? 'py-1 px-6' : 'py-1 px-3'}
       ${xl ? 'border-2' : 'border'}
-        focus:outline-none inline-block cursor-pointer rounded-md border-gray-900 bg-gray-900 text-gray-50 shadow-lg hover:border-opacity-10 dark:border-gray-50 dark:bg-gray-50 dark:text-gray-900  `}
+        focus:outline-none inline-block cursor-pointer rounded-md border-gray-900 bg-gray-900 text-gray-50 shadow-lg hover:border-opacity-10 dark:border-gray-50 dark:bg-gray-50 dark:text-gray-900`}
     >
       {children}
     </a>
