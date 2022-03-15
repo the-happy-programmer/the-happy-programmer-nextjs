1f20b99bf76f9d6b94ac529dd6ee202f855e0531
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-03-15 13:46:27 +0000

    gtag sorted

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import '../styles/global.css'
+import Script from 'next/script'
 import Sitelayout from '../widget/Sitelayout'
+const gtag = process.env.WORDPRESS_AUTH_REFRESH_TOKEN
 
 function MyApp({ Component, pageProps }) {
   return (
     <Sitelayout>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`} />
+      <Script>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${gtag});
+        `}
+      </Script>
       <Component {...pageProps} />
     </Sitelayout>
   )
