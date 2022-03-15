29cee216bab7c1d004d6460f56229004d94a26a8
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-03-15 16:15:29 +0000

    GTAG event added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import '../styles/global.css'
 import Script from 'next/script'
 import Sitelayout from '../widget/Sitelayout'
-const gtag = process.env.GTAG
 
 function MyApp({ Component, pageProps }) {
   return (
     <Sitelayout>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -15,7 +14,7 @@ function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', ${gtag});
+            gtag('config', ${process.env.GTAG});
         `}
       </Script>
       <Component {...pageProps} />
