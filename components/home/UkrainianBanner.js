b1888d3a040b169a3ff01280280a06f935f25aa5
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-07 22:53:20 +0100

    banner light mode

--- components/home/UkrainianBanner.js
+++ components/home/UkrainianBanner.js
@@ -5,11 +5,13 @@ const UkrainianBanner = ({ setsupp, svgname, title, subtitle, link }) => {
     <div className="spac flex w-full flex-row justify-between px-3 py-4">
       <SvgtoReact name="ukrainianflag" height={50} width={50} />
       <div className="mr-auto pl-6">
-        <p className="font-semibold dark:text-gray-100">{title}</p>
-        <p className="dark:text-gray-300">
+        <p className="font-semibold text-gray-700 dark:text-gray-100">
+          {title}
+        </p>
+        <p className="text-gray-600 dark:text-gray-300 ">
           {subtitle}{' '}
           <a
-            className="hover:underline dark:text-darkaccent"
+            className="text-accent hover:underline dark:text-darkaccent"
             href="https://www.unicef.org.uk/donate/donate-now-to-protect-children-in-ukraine/?gclid=Cj0KCQjwl7qSBhD-ARIsACvV1X0lPlYwu0E2vfVCEX3x6N4B_IkPi5SvQLlLF65pZgNEnWBTIbX_27caArikEALw_wcB"
           >
             {link}
@@ -21,7 +23,7 @@ const UkrainianBanner = ({ setsupp, svgname, title, subtitle, link }) => {
           setsupp(false)
         }}
         name={svgname}
-        className="cursor-pointer self-center fill-current dark:text-gray-300 dark:hover:text-gray-100"
+        className="cursor-pointer self-center fill-current hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100"
         height={20}
         width={20}
       />
