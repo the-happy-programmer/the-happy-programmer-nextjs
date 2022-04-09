eb35b5430d78b2cb75223e9de314e66710a8c41d
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-09 18:42:50 +0100

    svg bug fixed

--- components/CatTag.js
+++ components/CatTag.js
@@ -20,13 +20,16 @@ export default function CatTag({ categories, title, tags }) {
         {tags?.map((cat) => (
           <div className="py-2.5" key={cat.uri}>
             <Link href={`${cat.uri}`}>
-              <a className="flex cursor-pointer flex-row text-sm text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">
-                <SvgtoReact
-                  className="pr-3"
-                  name={cat.name.toLowerCase()}
-                  height={20}
-                />
-                {cat.name}
+              <a className="flex cursor-pointer flex-row  text-sm text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">
+                <div>
+                  <SvgtoReact
+                    className="pr-2"
+                    name={cat.name.toLowerCase()}
+                    height={20}
+                  />
+                </div>
+
+                <p className="overflow-hidden overflow-ellipsis">{cat.name}</p>
               </a>
             </Link>
           </div>
