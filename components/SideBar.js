5b2893bb8e664e26049294e47da6c3ac8e5f355e
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-14 20:31:17 +0100

    bg on mobile menu

--- components/SideBar.js
+++ components/SideBar.js
@@ -4,7 +4,7 @@ import SvgtoReact from './Svgtoreact'
 export default function SideBar({ links, setsidebar }) {
   return (
     <div className="absolute bottom-0 top-0 left-0 h-screen w-screen bg-gray-300 bg-opacity-95 dark:bg-gray-800">
-      <div className="h-screen w-72 border-r border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-200">
+      <div className="h-screen w-72 border-r border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800">
         <div className="flex flex-col items-center border-b px-3 py-5 dark:border-gray-700">
           <div
             className="cursor-pointer self-end"
