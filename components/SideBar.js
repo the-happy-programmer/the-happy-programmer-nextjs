5fab3d2dd1b0faba5655deda5bb8155430007c3a
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-14 20:53:00 +0100

    sidemenu

--- components/SideBar.js
+++ components/SideBar.js
@@ -4,8 +4,8 @@ import SvgtoReact from './Svgtoreact'
 export default function SideBar({ links, setsidebar }) {
   return (
     <div className="absolute bottom-0 top-0 left-0 h-screen w-screen bg-gray-300 bg-opacity-95 dark:bg-gray-800">
-      <div className="h-screen w-72 border-r border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800">
-        <div className="flex flex-col items-center border-b px-3 py-5 dark:border-gray-700">
+      <div className="flex h-screen w-72 flex-col border-r border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800">
+        <div className="flex flex-col items-center border-b px-3 py-4 dark:border-gray-700">
           <div
             className="cursor-pointer self-end"
             onClick={(e) => setsidebar(false)}
@@ -16,28 +16,28 @@ export default function SideBar({ links, setsidebar }) {
               height={15}
             />
           </div>
-          <div
-            className="cursor-pointer self-start"
-            onClick={(e) => setsidebar(false)}
-          >
-            <Link href="/">
-              <SvgtoReact
-                name="fulllogo"
-                className="fill-current stroke-current text-gray-900 dark:text-gray-50"
-                height={20}
-                width={75}
-              />
-            </Link>
-          </div>
         </div>
-        <div className="px-4">
+        <div className="px-5 py-6">
           {links.map(([link, name]) => (
-            <div className="py-2" key={name}>
+            <div className="py-3" key={name}>
               <Link href={link}>
                 <a onClick={(e) => setsidebar(false)}>{name}</a>
               </Link>
             </div>
           ))}
+        </div>
+        <div
+          className="mt-auto w-full cursor-pointer self-start border-t p-4 dark:border-gray-700 dark:bg-gray-800"
+          onClick={(e) => setsidebar(false)}
+        >
+          <Link href="/">
+            <SvgtoReact
+              name="fulllogo"
+              className="fill-current stroke-current text-gray-900 dark:text-gray-50"
+              height={20}
+              width={75}
+            />
+          </Link>
         </div>
       </div>
     </div>
