fc861b257eb969d3913a1aeb53bb5531f35ccd97
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-15 05:00:55 +0100

    home ui bugs

--- components/home/Technologies.js
+++ components/home/Technologies.js
@@ -4,7 +4,7 @@ import SvgtoReact from '../Svgtoreact'
 export default function Technologies({ title, subtitle, icons }) {
   return (
     <div className="border-b border-gray-200 py-20  dark:border-gray-700">
-      <div className="container flex flex-col place-content-center items-center justify-between px-3 sm:flex-col md:flex-row lg:flex-row xl:flex-row">
+      <div className="container flex flex-col place-content-center items-center justify-between gap-14 px-3 sm:flex-col md:flex-row lg:flex-row xl:flex-row">
         <div className="grid grid-cols-4 justify-items-center gap-10 sm:gap-10 md:gap-20 lg:gap-20 xl:gap-20">
           {icons.map((icon) => (
             <SvgtoReact
@@ -16,10 +16,12 @@ export default function Technologies({ title, subtitle, icons }) {
           ))}
         </div>
         <div className="w-80">
-          <p className="text-3xl font-bold dark:text-gray-50">{title}</p>
+          <p className="text-left text-3xl font-bold dark:text-gray-50">
+            {title}
+          </p>
           <p className="py-8 dark:text-gray-300">{subtitle}</p>
           <HappyLink href={'/about'} classes="dark:text-darkaccent text-accent">
-            <div className="flex flex-row items-center gap-x-3">
+            <div className="flex flex-row items-center gap-x-3 text-center">
               Learn more about our courses
               <SvgtoReact
                 name="arrow"
