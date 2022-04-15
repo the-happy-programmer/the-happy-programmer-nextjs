785ac3162bdc1f992eb3de3a2adb402f1b1d0f9c
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-15 03:17:21 +0100

    hero section bug

--- components/home/HeroCarousel.js
+++ components/home/HeroCarousel.js
@@ -3,12 +3,12 @@ import SvgtoReact from '../Svgtoreact'
 
 export default function HeroCarousel({ hero }) {
   return (
-    <div className="container flex flex-col-reverse items-center justify-between py-20 px-4 text-center sm:flex-col-reverse sm:text-center md:flex-row md:text-left lg:flex-row lg:text-left xl:flex-row xl:text-left">
+    <div className="container flex flex-col-reverse items-center justify-between py-8 px-4 text-center sm:flex-col-reverse sm:py-20 sm:text-center md:flex-row md:py-20 md:text-left lg:flex-row lg:py-20 lg:text-left xl:flex-row xl:py-20 xl:text-left">
       <div className="w-96">
-        <h1 className="text-4xl font-bold  dark:text-gray-100  sm:text-4xl  md:text-5xl lg:text-5xl xl:text-5xl">
+        <h1 className="text-4xl font-bold dark:text-gray-100  sm:text-4xl  md:text-5xl lg:text-5xl xl:text-5xl">
           {hero.title}
         </h1>
-        <p className="py-10 text-lg text-gray-600 dark:text-gray-200">
+        <p className="py-10 px-2 text-lg text-gray-600 dark:text-gray-200 sm:px-0 md:px-0 lg:px-0 xl:px-0">
           {hero.subtitle}
         </p>
         <HappyButton xl={true} href="#subscribe">
