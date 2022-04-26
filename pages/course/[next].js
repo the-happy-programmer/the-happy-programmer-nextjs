0b0c8592b5f25fc86834cc89e9a12a7d60454838
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-26 22:08:17 +0100

    cc

--- pages/course/[next].js
+++ pages/course/[next].js
@@ -2,7 +2,7 @@ import Content from '../../components/course/Content'
 import { getDocBySlug } from '../../lib/courseslib/api'
 import { markdownToHtml } from '../../lib/courseslib/htmlmarkdown'
 
-export default function Pag({ content }) {
+export default function Pag({ content, meta, slug }) {
   return (
     <>
       <Content content={content} />
@@ -15,7 +15,7 @@ export async function getStaticProps({ params }) {
   const { meta, slug, content } = await getDocBySlug(next, '/course/swift')
 
   const con = await markdownToHtml(content || '')
-  return { props: { content: con } }
+  return { props: { content: con, meta, slug } }
 }
 
 export async function getStaticPaths() {
