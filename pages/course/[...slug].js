c7d0565a11aed15619b4cc61f8918bd0adc9766b
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-04-29 21:25:30 +0100

    all dir

--- pages/course/[...slug].js
+++ pages/course/[...slug].js
@@ -12,13 +12,23 @@ export default function Pag({ content, meta, slug }) {
 
 export async function getStaticProps({ params }) {
   const { slug } = params
-  const { meta, link, content } = await getDocBySlug(slug, '/course/swift')
+  // console.log('params:', slug[0], slug[1])
+  const { meta, link, content } = await getDocBySlug(
+    slug[1],
+    `course/${slug[0]}`
+  )
+  // console.log('meta:', meta)
 
   const con = await markdownToHtml(content || '')
   return { props: { content: con, meta, link } }
 }
 
 export async function getStaticPaths() {
-  const paths = ['/course/swift', '/course/swiftui']
+  const getDirectories = (source) =>
+    readdirSync(source, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name)
+  console.log(getDirectories(`${process.cwd()}/course`))
+  const paths = ['/course/swift/swift', '/course/swiftui/swift']
   return { paths, fallback: false }
 }
