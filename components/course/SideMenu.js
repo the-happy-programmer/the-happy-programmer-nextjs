094ed794c7eca917ce6868f7f4d134c0dc82e899
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-05-23 22:58:07 +0100

    cc

--- components/course/SideMenu.js
+++ components/course/SideMenu.js
@@ -3,8 +3,6 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 
-//
-
 export default function SideMenu({ courseslugs }) {
   const [courses, setCourses] = useState(courseslugs)
   const router = useRouter()
