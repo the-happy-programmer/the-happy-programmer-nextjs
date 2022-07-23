1bc037500ee5b3ad6f42d93e7fd4066260302149
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-07-23 21:40:09 +0100

    cc

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,22 @@
 import NextAuth from 'next-auth'
 import FacebookProvider from 'next-auth/providers/facebook'
-import GithubProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
   providers: [
     FacebookProvider({
       clientId: process.env.FB_CLIENT_ID,
       clientSecret: process.env.FB_APP_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
   callbacks: {
