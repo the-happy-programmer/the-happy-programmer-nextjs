2fdb7d665f883862959e7ec5856f347eb9b019d8
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-07-30 23:11:32 +0100

    sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,8 +5,8 @@ import SvgtoReact from '../../components/Svgtoreact'
 export default function SignIn({ providers }) {
   return (
     <>
-      <div className="flex flex-row">
-        <div className="py-28">
+      <div className="space container flex flex-row items-center justify-between py-36">
+        <div>
           <h1 className="pb-5 text-3xl font-bold dark:text-gray-50">Sign In</h1>
           <div className="flex max-w-xs flex-col gap-y-4">
             {Object.values(providers).map((provider) => (
@@ -19,22 +19,23 @@ export default function SignIn({ providers }) {
             ))}
           </div>
         </div>
-        <div>
-          <SvgtoReact
-            name="fulllogo"
-            className="justify-self-auto fill-current stroke-current text-gray-500 dark:text-gray-400"
-            height={100}
-          />
-          <h2></h2>
-        </div>
-        <div className=" ml-auto">
-          <div className="left-0 top-0 bottom-0">
+        <div className="flex flex-col items-center text-gray-900 dark:text-gray-50">
+          <div className="pb-14">
             <SvgtoReact
-              name="slopediv"
-              height="90vh"
-              className="fill-current text-gray-800 dark:text-gray-200"
+              name="signinlogo"
+              height={40}
+              className="fill-current stroke-current"
             />
           </div>
+          <p className="">welcome to</p>
+          <h2 className="py-3 text-3xl font-bold ">The Happy Programmer</h2>
+          <p className="">
+            cource in every{' '}
+            <span className="text-accent dark:text-darkaccent">
+              {' '}
+              Technology
+            </span>
+          </p>
         </div>
       </div>
     </>
