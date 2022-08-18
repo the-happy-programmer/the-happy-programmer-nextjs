0532b0b6498179f24a4ac5a94dbdcdda1ff9432b
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-08-18 03:14:08 +0100

    ts transition 90%

--- components/about/DisplayCard.tsx
+++ components/about/DisplayCard.tsx
@@ -13,7 +13,11 @@ export default function DisplayCard({
       className="flex transform cursor-pointer flex-col rounded-md border border-gray-900 border-opacity-5 bg-gray-50 p-5 transition duration-150 ease-in hover:border-gray-50 hover:shadow-lg dark:border-gray-50 dark:border-opacity-20 dark:bg-gray-900 dark:hover:border-gray-900"
     >
       <div className="h-16 pt-5 pb-2">
-        <SvgtoReact name={svg.toLowerCase()} height={socials ? 25 : 30} />
+        <SvgtoReact
+          name={svg.toLowerCase()}
+          className="fill-current text-gray-800 dark:text-gray-100"
+          height={socials ? 25 : 30}
+        />
       </div>
       <p className="text-lg font-bold capitalize dark:text-gray-50">{svg}</p>
       <p className="py-2 text-sm leading-loose text-gray-600 dark:text-gray-300">
