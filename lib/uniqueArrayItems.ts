bc707769be329e894eacedb9f178208d02553827
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-09-03 09:56:01 +0100

    cc

---
+++ lib/uniqueArrayItems.ts
@@ -0,0 +1,17 @@
+import { getAllDocs } from './courseslib/courseapi'
+import type { PostProps } from './types/blog'
+
+interface UniqueArrayItems {
+  categories: string[]
+  tags: string[]
+}
+
+export const uniqueArrayItems = (): UniqueArrayItems => {
+  const posts = getAllDocs('course/blog')
+  const cat = posts.map((doc: PostProps) => doc.meta.categories)
+  const tag = posts.map((doc: PostProps) => doc.meta.tags)
+  return {
+    categories: [...new Set(cat.concat.apply([], cat))],
+    tags: [...new Set(tag.concat.apply([], tag))],
+  }
+}
