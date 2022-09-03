fc5fe737c0674979249c3c893829023db1412441
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-09-03 08:46:18 +0100

    cc

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -1,6 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { unstable_getServerSession } from 'next-auth/next'
 import { authOptions } from './auth/[...nextauth]'
-export default async (req, res) => {
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<{ content?: string; error?: string }>
+) => {
   const session = await unstable_getServerSession(req, res, authOptions)
   if (session) {
     res.send({
---
+++ pages/api/subscribe.ts
@@ -0,0 +1,23 @@
+import mailchimp from '@mailchimp/mailchimp_marketing'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER,
+})
+
+export default async function subscribe(
+  req: NextApiRequest,
+  res: NextApiResponse<{ error: string }>
+) {
+  const { email } = req.body
+  try {
+    await mailchimp.lists.addListMember(process.env.AUDIENCE_ID, {
+      email_address: email,
+      status: 'subscribed',
+    })
+    return res.status(201).json({ error: '' })
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() })
+  }
+}
