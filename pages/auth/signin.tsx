8bc4ab9574c4e121f190afc8d48334ffb90c0764
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-09-07 10:32:19 +0100

    cc

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -4,6 +4,8 @@ import SvgtoReact from '../../components/Svgtoreact'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { GetServerSideProps } from 'next'
+import style from '../../styles/buttons.module.css'
+
 export default function SignIn({
   providers,
 }: {
@@ -25,7 +27,7 @@ export default function SignIn({
           className="stroke-current text-gray-900 dark:text-gray-900"
         />
       </div>
-      <div className="mx-auto mt-10 max-w-sm rounded-md border border-gray-50 border-opacity-10 bg-gray-900 bg-opacity-10 px-6 py-10 backdrop-blur-lg backdrop-filter">
+      <div className={style.signin}>
         <h1 className="pb-5 text-xl font-bold text-gray-50">
           Sign in with your email
         </h1>
@@ -41,15 +43,17 @@ export default function SignIn({
           <input
             type="email"
             name=""
-            className="w-full rounded-md border border-opacity-10 bg-gray-50 bg-opacity-10 py-4 pr-5 pl-12 text-gray-50 shadow-sm"
+            className="w-full rounded-md border border-opacity-10 bg-gray-50 bg-opacity-5 py-2 pr-5 pl-12 text-gray-50 shadow-sm"
             placeholder="Email Adress"
           />
         </div>
-        <button className="focus:outline-none inline-block w-full cursor-pointer rounded-md border border-gray-50  bg-gray-50 py-3 px-4 text-xs font-medium capitalize leading-6 tracking-wide text-gray-900 shadow-lg hover:border-opacity-10 hover:bg-opacity-80">
+        <button className="focus:outline-none inline-block w-full cursor-pointer rounded-md border border-gray-50  bg-gray-50 py-2 px-4 text-xs font-medium capitalize leading-6 tracking-wide text-gray-900 shadow-lg hover:border-opacity-10 hover:bg-opacity-80">
           Sign in
         </button>
-        <div className="py-10">
-          <div className="border-b border-opacity-10" />
+        <div className="my-6 flex items-center">
+          <div className="mr-3 flex-grow border-t border-gray-600"></div>
+          <div className="text-gray-400">Or</div>
+          <div className="ml-3 flex-grow border-t border-gray-600"></div>
         </div>
         <div className="flex flex-row justify-around">
           {providers &&
