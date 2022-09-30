a920f29be9b3fd7b0a9f5404e4ae45ca3f9a97bc
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-09-30 15:51:08 +0100

    cc

--- components/home/subscribe/NewSupport.tsx
+++ components/home/subscribe/NewSupport.tsx
@@ -1,64 +1,83 @@
-import HappyButton from '../../Happybutton'
 import SubDesc from './SubDesc'
+import style from '../../../styles/buttons.module.css'
+import SvgtoReact from '../../Svgtoreact'
 
 export default function NewSupport({ support, supportDesc }): JSX.Element {
   return (
-    <div className="container flex flex-col border-b py-20">
-      <div className="mx-auto max-w-xs py-20 text-center">
-        <p className="py-2 text-sm font-semibold uppercase text-accent dark:text-darkaccent">
-          full-featured
-        </p>
-        <h3 className="text-3xl font-bold">Become an expert in programming</h3>
-      </div>
-      <div className="flex flex-row py-10">
-        <div className="flex w-1/2 flex-col content-around gap-y-10 border-r border-gray-900 border-opacity-10">
-          <SubDesc title="Apple products development" icon="support/swift">
-            <div>
-              Swift is the main language for creating Apps in <a>iOS</a>,
-              <a>PadOS</a>, <a>MacOS</a> and
-              <a>WatchOS</a>
-            </div>
-          </SubDesc>
-          <SubDesc title="Web Development" icon="support/mobile">
-            <div>
-              Courses in React Nexts. NuxtJS. Javascript is being taugh in
-              detail in THP
-            </div>
-          </SubDesc>
-          <SubDesc title="Mobile Development" icon="support/web">
-            <div>
-              The fastest growing market mobile development is being taught and
-              difficult concepts explained
-            </div>
-          </SubDesc>
-          <SubDesc title="Server Development" icon="support/server">
-            <div>
-              To become and all around develoner you need to learn to create
-              servers, here is the right place.
-            </div>
-          </SubDesc>
+    <div className="bg-gray-100 dark:bg-gray-800">
+      <div className="container flex flex-col border-b py-20">
+        <div className="mx-auto max-w-xs pb-10 pt-20 text-center">
+          <p className="py-2 text-sm font-semibold uppercase text-accent dark:text-darkaccent">
+            full-featured
+          </p>
+          <h3 className="text-3xl font-bold dark:text-gray-50">
+            Become an expert in programming
+          </h3>
         </div>
-        <div className="flex w-1/2 flex-col text-center">
-          <p className="text-3xl font-bold">£5</p>
-          <p className="text-gray-900 text-opacity-60 dark:text-gray-50">
-            per month
-          </p>
-          <div className="mx-auto grid max-w-sm grid-cols-2 gap-1 py-16">
-            {[
-              'Early Access',
-              'Sub-only Courses',
-              'Answer Questions',
-              'Share source code',
-              'Personal Mentoring',
-              'Complete Projects',
-            ].map((item) => (
-              <p className="flex-none bg-blue-50 p-3 text-sm">{item}</p>
-            ))}
-            <button className="">Subscribe</button>
+        <div className="flex flex-row pt-10 pb-32">
+          <div className="flex w-1/2 flex-col justify-center gap-y-10 border-r border-gray-900 border-opacity-10">
+            <SubDesc title="Apple products development" icon="support/swift">
+              <div>
+                Swift is the main language for creating Apps in <a>iOS</a>,
+                <a>PadOS</a>, <a>MacOS</a> and
+                <a>WatchOS</a>
+              </div>
+            </SubDesc>
+            <SubDesc title="Web Development" icon="support/web">
+              <div>
+                Courses in React Nexts. NuxtJS. Javascript is being taugh in
+                detail in THP
+              </div>
+            </SubDesc>
+            <SubDesc title="Mobile Development" icon="support/mobile">
+              <div>
+                The fastest growing market mobile development is being taught
+                and difficult concepts explained
+              </div>
+            </SubDesc>
+            <SubDesc title="Server Development" icon="support/server">
+              <div>
+                To become and all around develoner you need to learn to create
+                servers, here is the right place.
+              </div>
+            </SubDesc>
           </div>
-          <p className="text-gray-900 text-opacity-60 dark:text-gray-50">
-            Try THP subscription now
-          </p>
+          <div className="mx-auto flex w-1/2 max-w-sm flex-col text-center">
+            <p className="text-3xl font-bold dark:text-gray-50">£5</p>
+            <p className="text-gray-900 text-opacity-60 dark:text-gray-50">
+              per month
+            </p>
+            <div className="mx-auto grid w-full grid-cols-2 gap-2 py-16">
+              {[
+                'Early Access',
+                'Sub-only Courses',
+                'Answer Questions',
+                'Share source code',
+                'Personal Mentoring',
+                'Complete Projects',
+              ].map((item) => (
+                <div className="flex-none rounded-md bg-gray-50 p-3 text-sm">
+                  <div className="flex flex-row">
+                    <SvgtoReact
+                      name="check"
+                      height={20}
+                      width={20}
+                      className="stroke-current text-gray-900"
+                    />
+                    <p className="mx-auto text-center font-light text-gray-900 text-opacity-60">
+                      {item}
+                    </p>
+                  </div>
+                </div>
+              ))}
+              <div className="col-span-2 pt-3">
+                <button className={style.fullbtn}>Get Started today</button>
+              </div>
+            </div>
+            <p className="text-sm text-gray-900 text-opacity-50 dark:text-gray-50">
+              Try THP subscription now
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- components/home/subscribe/SubDesc.tsx
+++ components/home/subscribe/SubDesc.tsx
@@ -11,12 +11,17 @@ export default function SubDesc({
 }): JSX.Element {
   return (
     <div className="flex flex-row gap-x-5">
-      <SvgtoReact name={icon} height={40} width={40} />
+      <SvgtoReact
+        name={icon}
+        className="h-15 w-15 fill-current text-gray-800 dark:text-gray-50"
+        height={50}
+        width={50}
+      />
       <div className="flex flex-col">
-        <p className="font-semibold text-accent dark:text-darkaccent">
+        <p className="text-sm font-semibold text-accent dark:text-darkaccent">
           {title}
         </p>
-        <div className="max-w-sm text-gray-900 text-opacity-60 dark:text-gray-50">
+        <div className="max-w-sm text-sm font-light text-gray-900 text-opacity-80 dark:text-gray-50">
           {children}
         </div>
       </div>
