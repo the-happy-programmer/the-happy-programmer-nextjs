c5e3e3291cb5812d81939c1f9f3fc175d737d202
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-12-19 12:50:47 +0000

    cc

--- components/Mdx-remote.tsx
+++ components/Mdx-remote.tsx
@@ -1,15 +1,13 @@
+import fs from 'node:fs';
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import {
-  BUNDLED_LANGUAGES,
-  getHighlighter,
-  type HighlighterOptions,
-} from 'shiki'
+import { getHighlighter } from 'shiki'
+import rehypeSlug from 'rehype-slug'
 
 import Image from 'next/image'
 import rehypePrism from 'rehype-prism-plus'
 import rehypeCodeTitles from 'rehype-code-titles'
 import rehypePrettyCode from 'rehype-pretty-code'
-import oneDarkPro from 'shiki/themes/one-dark-pro.json'
+import moonlightII from "../assets/moonlight-ii.json"
 
 const components = {
   img: (props: any) => (
@@ -26,17 +24,15 @@ const components = {
 export async function CustomMDX(props: any) {
   const options = {
     keepBackground: false,
-    theme: oneDarkPro,
-    highlighter: await getHighlighter({
-      theme: 'one-dark-pro',
-    }),
-  }
+    theme: moonlightII,
+
+  };
   return (
     <MDXRemote
       {...props}
       options={{
         mdxOptions: {
-          rehypePlugins: [rehypePrettyCode],
+          rehypePlugins: [[rehypePrettyCode, options], rehypeSlug ],
         },
       }}
       components={{ ...components, ...(props.components || {}) }}
