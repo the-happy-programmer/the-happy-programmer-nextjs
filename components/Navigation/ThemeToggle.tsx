bc473f7cd77273ef7d21bb93a529109af078625b
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-12-21 07:28:24 +0000

    cc

---
+++ components/Navigation/ThemeToggle.tsx
@@ -0,0 +1,12 @@
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/dropdown";
+
+const ThemeToggle = () => {
+  return <Dropdown></Dropdown>;
+};
+
+export default ThemeToggle;
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,9 +1,14 @@
-'use client'
+"use client";
 
-import {NextUIProvider} from '@nextui-org/react'
-import { useRouter } from 'next/navigation'
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/navigation";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter()
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
-}
+  const router = useRouter();
+  return (
+    <NextUIProvider navigate={router.push}>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
+    </NextUIProvider>
+  );
+};
