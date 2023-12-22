f17c42b2eac6a9891caf32f282f2304ae5bb8dfd
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-12-22 18:54:03 +0000

    t

--- components/home/subscribe/NewSupport.tsx
+++ components/home/subscribe/NewSupport.tsx
@@ -3,9 +3,10 @@ import SubDesc from "@/components/home/subscribe/SubDesc";
 import style from "@/styles/buttons.module.css";
 import Check from "@/public/svg/check.svg";
 import { useState } from "react";
-import Link from "next/link";
+import { Link } from "@nextui-org/link";
 import { Chip } from "@nextui-org/chip";
 import { Tab, Tabs } from "@nextui-org/tabs";
+import { Button } from "@nextui-org/button";
 
 export default function NewSupport({}): JSX.Element {
   const [active, setActive] = useState<string>("month");
@@ -20,7 +21,7 @@ export default function NewSupport({}): JSX.Element {
               content: "drop-shadow shadow-black text-white",
             }}
           >
-            Become an expert in programming
+            FULL-FEATURED
           </Chip>
           <h3 className="text-3xl font-bold">
             Become an expert in programming
@@ -37,8 +38,8 @@ export default function NewSupport({}): JSX.Element {
           <Tab key="year" title="Year" />
           <Tab key="month" title="Month" />
         </Tabs>
-        <div className="flex flex-col pb-32 pt-unit-xl sm:flex-col md:flex-row lg:flex-row xl:flex-row">
-          <div className="flex flex-col items-center gap-y-10 border-r border-default-900 border-opacity-10 md:w-1/2 md:justify-center lg:w-1/2 lg:justify-center xl:w-1/2 xl:justify-center">
+        <div className="flex flex-col justify-between pb-unit-4xl pt-unit-xl sm:flex-col md:flex-row lg:flex-row xl:flex-row">
+          <div className="flex flex-col items-center gap-y-unit-xl  md:w-1/2 md:justify-center lg:w-1/2 lg:justify-center xl:w-1/2 xl:justify-center">
             <SubDesc title="Apple products development" icon="support/swift">
               <div>
                 Swift is the main language for creating Apps in{" "}
@@ -148,14 +149,81 @@ export default function NewSupport({}): JSX.Element {
               </div>
             </SubDesc>
           </div>
-          <div className="mx-auto flex w-1/2 max-w-sm flex-col pt-10 text-center sm:pt-10 md:pt-0 lg:pt-0 xl:pt-0">
+          <div className="mx-auto w-full max-w-sm">
+            <div className="rounded-2xl border border-indigo-600 p-6 shadow-sm ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12">
+              <div className="text-center">
+                <div className="text-lg font-medium">
+                  <Chip
+                    variant="shadow"
+                    classNames={{
+                      base: "bg-gradient-to-br from-primary to-secondary border-white/50 shadow-secondary/30",
+                      content: "drop-shadow shadow-black text-white",
+                    }}
+                  >
+                    Pro
+                  </Chip>
+                  <span className="sr-only">Plan</span>
+                </div>
+
+                <p className="mt-2 sm:mt-4">
+                  <strong className="text-3xl font-bold  sm:text-4xl">
+                    {" "}
+                    {active === "month" ? "£5" : "£50"}{" "}
+                  </strong>
+
+                  <span className="text-sm font-medium text-default-600">
+                    {active === "month" ? "/month" : "/year"}
+                  </span>
+                </p>
+              </div>
+
+              <ul className="mt-6 space-y-2">
+                {[
+                  "Early Access",
+                  "Sub-only Courses",
+                  "Answer Questions",
+                  "Share source code",
+                  "Personal Mentoring",
+                  "Complete Projects",
+                ].map((item) => (
+                  <li className="flex items-center gap-2" key={item}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-5 w-5 text-primary"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 12.75l6 6 9-13.5"
+                      />
+                    </svg>
+                    <span className="text-default-700">{item}</span>
+                  </li>
+                ))}
+              </ul>
+              <div className="pt-unit-xl">
+                <Button
+                  variant="shadow"
+                  as={Link}
+                  className="w-full border-white/50 bg-gradient-to-br from-primary to-secondary text-white shadow-secondary/30"
+                >
+                  Get Started
+                </Button>
+              </div>
+            </div>
+          </div>
+          {/* <div className="mx-auto flex w-1/2 max-w-sm flex-col pt-10 text-center sm:pt-10 md:pt-0 lg:pt-0 xl:pt-0">
             <p className="text-3xl font-bold dark:text-stone-50">
               {active === "month" ? "£5" : "£50"}
             </p>
-            <p className="text-default-900 text-opacity-60 dark:text-stone-50">
+            <p className="text-default-600">
               {active === "month" ? "per month" : "per year"}
             </p>
-            <div className="mx-auto grid w-full grid-cols-2 gap-2 py-16">
+            <div className="mx-auto grid w-full grid-cols-2 gap-2 py-unit-xl">
               {[
                 "Early Access",
                 "Sub-only Courses",
@@ -166,7 +234,7 @@ export default function NewSupport({}): JSX.Element {
               ].map((item) => (
                 <div
                   key={item}
-                  className="flex-none rounded-md bg-stone-50 p-3 text-sm dark:bg-stone-700 "
+                  className="flex-none rounded-md bg-default-100 p-3 text-sm"
                 >
                   <div className="flex flex-row">
                     <Check
@@ -186,10 +254,8 @@ export default function NewSupport({}): JSX.Element {
                 </Link>
               </div>
             </div>
-            <p className="text-sm text-default-900 text-opacity-50 dark:text-stone-50">
-              Try THP subscription now
-            </p>
-          </div>
+            <p className="text-sm text-default-600">Try THP subscription now</p>
+          </div> */}
         </div>
       </div>
     </div>
