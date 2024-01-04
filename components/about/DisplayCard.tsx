36a8d1f89647174dafacd967567a90a39417529c
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-01-04 18:43:48 +0000

    cc

--- components/about/DisplayCard.tsx
+++ components/about/DisplayCard.tsx
@@ -1,6 +1,6 @@
-import SvgtoReact from '../Svgtoreact'
-import HappyLink from '../HappyLink'
-import type { DisplayCardProps } from '../../lib/types/about'
+import SvgtoReact from '@/components/Svgtoreact';
+import HappyLink from '@/components/HappyLink';
+import type { DisplayCardProps } from '@/lib/types/about';
 export default function DisplayCard({
   svg,
   desc,
@@ -10,9 +10,9 @@ export default function DisplayCard({
   return (
     <div
       key={svg}
-      className="flex transform cursor-pointer flex-col rounded-md border border-default-900 border-opacity-5 bg-stone-50 p-5 transition duration-150 ease-in hover:border-stone-50 hover:shadow-lg dark:border-stone-50 dark:border-opacity-20 dark:bg-default-900 dark:hover:border-default-900"
+      className="flex transform cursor-pointer flex-col rounded-md border border-default-900 border-opacity-5 p-5 transition duration-150 ease-in hover:border-stone-50 hover:shadow-lg dark:border-stone-50 dark:border-opacity-20 dark:bg-default-900 dark:hover:border-default-900"
     >
-      <div className="h-16 pt-5 pb-2">
+      <div className="h-16 pb-2 pt-5">
         <SvgtoReact
           name={svg.toLowerCase()}
           className="fill-current text-stone-800 dark:text-stone-100"
@@ -30,10 +30,10 @@ export default function DisplayCard({
         {socials ? 'Follow' : 'Projects'}{' '}
         <SvgtoReact
           name="arrow"
-          className="-rotate-90 transform fill-current group-hover:text-accent dark:text-stone-50 dark:group-hover:text-darkaccent "
+          className="group-hover:text-accent dark:group-hover:text-darkaccent -rotate-90 transform fill-current dark:text-stone-50 "
           height={15}
         />
       </HappyLink>
     </div>
-  )
+  );
 }
--- widget/DisplayInfo.tsx
+++ widget/DisplayInfo.tsx
@@ -1,24 +1,24 @@
-import { ReactNode } from 'react'
+import { ReactNode } from 'react';
 
 export default function DisplayInfo({
   title,
   subtitle,
   children,
 }: {
-  title: string
-  subtitle: string
-  children: ReactNode
+  title: string;
+  subtitle: string;
+  children: ReactNode;
 }): JSX.Element {
   return (
-    <div className=" bg-stone-100 pb-5 dark:bg-stone-800">
-      <p className="container px-3 py-6 text-2xl text-stone-800 dark:text-stone-50">
+    <div className="pb-5">
+      <p className="container px-3 py-6 text-2xl">
         {title}
         {'.'}
-        <span className="text-stone-500 dark:text-stone-400">{subtitle}</span>
+        <span className="text-default-500">{subtitle}</span>
       </p>
-      <div className="container grid grid-cols-1 gap-5 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-4">
+      <div className="container grid grid-cols-1 gap-5 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {children}
       </div>
     </div>
-  )
+  );
 }
