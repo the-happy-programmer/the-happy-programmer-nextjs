6674bfb89c0a7dd7003ebb2d515c1b930f0fa693
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-01-07 02:18:21 +0000

    cc

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,21 +1,22 @@
-import Table from '@/components/profile/Table'
-import Link from 'next/link'
-import styles from '@/styles/buttons.module.css'
-import { createClient } from '@/lib/utils/supabase/server'
-import { cookies } from 'next/headers'
-import PencilIcon from '@/public/svg/pencil.svg'
-import { redirect } from 'next/navigation'
+import Table from '@/components/profile/Table';
+import { createClient } from '@/lib/utils/supabase/server';
+import { cookies } from 'next/headers';
+import PencilIcon from '@/public/svg/pencil.svg';
+import { redirect } from 'next/navigation';
+import { Button } from '@nextui-org/button';
+import { Link } from '@nextui-org/link';
+import { HiMiniPencil } from 'react-icons/hi2';
 
 export default async function Profile() {
-  const subscription = false
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
+  const subscription = false;
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   return (
-    <div className="bg-stone-50 dark:bg-default-900">
+    <div className="">
       <div className="container ">
         <div className="mx-auto max-w-md py-20 text-default-900 dark:text-stone-50">
           <h1 className="text-3xl font-bold ">Profile</h1>
@@ -25,26 +26,33 @@ export default async function Profile() {
                 <p className="max-w-xs font-medium">{user?.email || ''}</p>
                 <form
                   action={async () => {
-                    'use server'
-                    const cookieStore = cookies()
-                    const supabase = createClient(cookieStore)
-                    await supabase.auth.signOut()
-                    return redirect('/signin')
+                    'use server';
+                    const cookieStore = cookies();
+                    const supabase = createClient(cookieStore);
+                    await supabase.auth.signOut();
+                    return redirect('/signin');
                   }}
                 >
-                  <button className={styles.sbtn}>Sign Out</button>
+                  <Button
+                    disableRipple
+                    type="submit"
+                    variant="solid"
+                    color="primary"
+                  >
+                    Sign Out
+                  </Button>
                 </form>
               </div>
               <div className="flex flex-row items-center justify-between py-3">
                 <p className="max-w-xs font-medium">Passowrd:*******</p>
-                <Link href="/profile/resetpassword" passHref>
+                <Button isIconOnly as={Link} href="/profile/resetpassword">
                   <div className="group cursor-pointer rounded-full border-2 border-default-900 border-opacity-40 p-1.5 hover:border-opacity-0 dark:border-stone-50">
                     <PencilIcon
                       className="stroke-curren text-default-900 text-opacity-40 group-hover:text-opacity-0 dark:text-stone-50"
                       height={16}
                     />
                   </div>
-                </Link>
+                </Button>
               </div>
             </Table>
             <Table title="Subscription">
@@ -55,18 +63,18 @@ export default async function Profile() {
                   ) : (
                     subscription
                   )}
-                  <Link href={'/support'} className={styles.sbtn}>
+                  <Button href={'/support'} disableRipple as={Link}>
                     Choose A Plan
-                  </Link>
+                  </Button>
                 </>
               </div>
             </Table>
-            <button className="rounded border border-stone-200 bg-default-900 bg-opacity-0 py-2.5 px-4 font-medium text-danger hover:border-opacity-80 hover:bg-opacity-5 hover:text-opacity-80 dark:border-stone-700 dark:bg-stone-50">
-              Delete Account
-            </button>
+            <Button disableRipple color="danger">
+              Delete Your Account
+            </Button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
