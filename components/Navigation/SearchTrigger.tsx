e2160af88a7d6a0d0e48f649f6a9b463d8bcd8f1
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-01-08 03:38:21 +0000

    cc

--- components/Navigation/SearchTrigger.tsx
+++ components/Navigation/SearchTrigger.tsx
@@ -4,6 +4,8 @@ import SvgtoReact from '../Svgtoreact';
 import { Kbd } from '@nextui-org/kbd';
 import FullLogo from '@/public/svg/fulllogo.svg';
 import { HiMiniMagnifyingGlass } from 'react-icons/hi2';
+import { HiArrowLongRight } from 'react-icons/hi2';
+import Github from '@/public/svg/github.svg';
 import {
   Modal,
   ModalContent,
@@ -15,14 +17,15 @@ import {
 import { useState } from 'react';
 import { PostProps } from '@/lib/types/blog';
 import { Input } from '@nextui-org/input';
+import { Link } from '@nextui-org/link';
 
 const SearchTrigger = ({ posts }: { posts: PostProps[] }) => {
-  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchList, setSearchList] = useState<PostProps[]>([]);
+  const [searchList, setSearchList] = useState<PostProps[]>(posts);
   const changeInput = (e: { target: { value: string } }) => {
     if (e.target.value === '') {
-      setSearchList([]);
+      setSearchList(posts);
       setSearchQuery(e.target.value);
       return;
     }
@@ -37,11 +40,9 @@ const SearchTrigger = ({ posts }: { posts: PostProps[] }) => {
         el.meta.title?.toLowerCase().includes(query)
     );
   };
-  const icontitle = (tag: string[]) =>
+  const IconTitle = ({ tag }: { tag: string[] }) =>
     tag.map((tag) => (
-      <div key={tag}>
-        <SvgtoReact height={20} width={20} name={tag.toLowerCase()} />
-      </div>
+      <SvgtoReact key={tag} height={25} width={25} name={tag.toLowerCase()} />
     ));
 
   return (
@@ -63,12 +64,13 @@ const SearchTrigger = ({ posts }: { posts: PostProps[] }) => {
         backdrop="blur"
         onOpenChange={onOpenChange}
         scrollBehavior="inside"
-        className="bg-default-100/20 text-xl"
+        className="border border-divider bg-default-100/50 text-lg"
         radius="lg"
         classNames={{
-          body: 'p-0 gap-0',
+          body: 'p-0 gap-0 flex flex-col gap-y-unit-sm',
           header: 'p-0',
-          footer: 'flex flex-row items-center justify-between',
+          footer:
+            'flex flex-row items-center justify-between border-t border-divider',
         }}
         shouldBlockScroll={true}
         hideCloseButton={true}
@@ -79,9 +81,10 @@ const SearchTrigger = ({ posts }: { posts: PostProps[] }) => {
               <ModalBody>
                 <ModalHeader>
                   <Input
+                    onChange={(e) => changeInput(e)}
                     radius="lg"
                     variant="flat"
-                    className=""
+                    placeholder="search the happy programmer"
                     classNames={{
                       input: [
                         'focus:bg-transparent',
@@ -92,21 +95,64 @@ const SearchTrigger = ({ posts }: { posts: PostProps[] }) => {
                       innerWrapper: ['bg-transparent', 'hover:bg-transparent'],
                       inputWrapper: [
                         'bg-transparent',
-                        'bg-default-100/20',
-                        'hover:bg-default-100/20',
-                        'group-data-[focus=true]:bg-default-100/20',
+                        'bg-default-100/0',
+                        'hover:bg-default-100/0',
+                        'group-data-[focus=true]:bg-default-100/0',
                         '!cursor-text',
-                        'group-data-[hover=true]:bg-default-100/20',
+                        'group-data-[hover=true]:bg-default-100/0',
                         'rounded-b-none',
+                        'shadow-none',
+                        'border-divider border-b',
                       ],
                     }}
                     startContent={<HiMiniMagnifyingGlass />}
                     endContent={<Kbd keys={['escape']} />}
                   />
                 </ModalHeader>
-                <ModalBody>{JSON.stringify(posts)}</ModalBody>
+                <ModalBody>
+                  {searchList.length === 0 && (
+                    <div className="flex flex-col items-center justify-center p-unit-xl">
+                      <p className="text-lg">
+                        no resaults for &quot;{searchQuery}&quot;
+                      </p>
+                      <p className="">Search something else</p>
+                    </div>
+                  )}
+                  {searchList.map((e) => (
+                    <div key={e.meta.title} className="px-unit-sm">
+                      <Button
+                        href={e.link}
+                        onClick={onClose}
+                        className="flex h-auto w-full flex-row justify-between bg-default-400/50 shadow-sm dark:bg-default-300/50"
+                        as={Link}
+                        disableRipple
+                        color="default"
+                        variant="flat"
+                        startContent={<IconTitle tag={e.meta.categories} />}
+                        endContent={<HiArrowLongRight />}
+                      >
+                        <div className="mr-auto flex flex-col gap-y-unit-1 py-unit-sm pl-unit-xs">
+                          <p className="max-w-lg text-ellipsis  text-xs text-default-600">
+                            {e.meta.description}
+                          </p>
+                          <p className="">{e.meta.title}</p>
+                        </div>
+                      </Button>
+                    </div>
+                  ))}
+                </ModalBody>
                 <ModalFooter>
-                  <p>whatever</p>
+                  <Button
+                    as={Link}
+                    className=""
+                    variant="ghost"
+                    color="default"
+                    disableRipple
+                    startContent={<Github className="h-unit-md" />}
+                    href="https://github.com/MyNameIsBond"
+                  >
+                    github
+                  </Button>
                   <FullLogo
                     height={20}
                     width={80}
