54d56d04b71a76b951f40a7c70d3fa12484d179d
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-05-28 02:32:00 +0100

    cc

--- components/magilink/MagicLinkForm.tsx
+++ components/magilink/MagicLinkForm.tsx
@@ -17,27 +17,36 @@ const SubmitButton = ({}) => {
 const MagicLinkForm = () => {
   const [message, formAction] = useFormState(magiclink, null)
   return (
-    <form
-      className="mx-auto flex w-full max-w-sm flex-col gap-y-unit-lg pt-unit-lg"
-      action={formAction}
-    >
-      <Input
-        isRequired
-        type="email"
-        label="Email"
-        name="email"
-        placeholder="you@example.com"
-        labelPlacement="outside"
-        isInvalid={message?.issues ? true : false}
-        errorMessage={
-          message?.issues ? message?.issues.map((issue: string) => issue) : null
-        }
-        endContent={
-          <HiEnvelope className="pointer-events-none flex-shrink-0 text-default-400" />
-        }
-      />
-      <SubmitButton />
-    </form>
+    <>
+      <form
+        className="mx-auto flex w-full max-w-sm flex-col gap-y-unit-lg pt-unit-lg"
+        action={formAction}
+      >
+        <Input
+          isRequired
+          type="email"
+          label="Email"
+          name="email"
+          placeholder="you@example.com"
+          labelPlacement="outside"
+          isInvalid={message?.issues ? true : false}
+          errorMessage={
+            message?.issues
+              ? message?.issues.map((issue: string) => issue)
+              : null
+          }
+          endContent={
+            <HiEnvelope className="pointer-events-none flex-shrink-0 text-default-400" />
+          }
+        />
+        <SubmitButton />
+      </form>
+      {message?.message === 'success' && (
+        <div className="mt-unit-lg text-center">
+          Check your email for the magic link
+        </div>
+      )}
+    </>
   )
 }
 
