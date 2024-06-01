b915231b50c879d232f10ca21eae8c8f11279e58
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-06-01 18:00:17 +0100

    cc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-const { nextui } = require('@nextui-org/react')
+import { nextui } from '@nextui-org/react'
 
 module.exports = {
   content: [
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './widget/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{ts,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class',
   theme: {
