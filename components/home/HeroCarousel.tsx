28fb0b6314bf2c581bc540798b8e00564e236396
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-06-03 11:13:38 +0100

    cc

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,6 +1,6 @@
-import type { TitleSubtitle } from "@/lib/types/home";
-import HeroCode from "@/public/svg/herocode.svg";
-import { Button } from "@nextui-org/button";
+import type { TitleSubtitle } from '@/lib/types/home'
+import HeroCode from '@/public/svg/herocode.svg'
+import { Button } from '@nextui-org/button'
 export default function HeroCarousel({
   title,
   subtitle,
@@ -15,16 +15,16 @@ export default function HeroCarousel({
           className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-secondary to-primary opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           style={{
             clipPath:
-              "polygon(94% 12%, 93% 99%, 49% 59%, 0% 100%, 48% 39%, 86% 78%)",
+              'polygon(94% 12%, 93% 99%, 49% 59%, 0% 100%, 48% 39%, 86% 78%)',
           }}
         />
       </div>
       <div className="container flex flex-col-reverse items-center justify-between px-4 py-8 text-center sm:flex-col-reverse sm:py-20 sm:text-center md:flex-row md:py-20 md:text-left lg:flex-row lg:py-20 lg:text-left xl:flex-row xl:py-20 xl:text-left">
         <div className="w-96">
-          <h1 className="inline text-4xl font-semibold tracking-tight lg:text-5xl">
+          <h1 className="bg-opacity-50 bg-gradient-to-b from-default-900 to-default-500 bg-clip-text text-4xl font-bold text-transparent md:text-5xl">
             {title}
           </h1>
-          <p className="px-2 py-8 text-lg text-default-500  dark:text-stone-200 sm:px-0 md:px-0 lg:px-0 xl:px-0">
+          <p className="px-2 py-8 text-lg text-default-400 sm:px-0 md:px-0 lg:px-0 xl:px-0">
             {subtitle}
           </p>
           <Button
@@ -51,10 +51,10 @@ export default function HeroCarousel({
           className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-secondary to-secondary opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           style={{
             clipPath:
-              "polygon(94% 12%, 93% 99%, 49% 59%, 0% 100%, 48% 39%, 86% 78%)",
+              'polygon(94% 12%, 93% 99%, 49% 59%, 0% 100%, 48% 39%, 86% 78%)',
           }}
         />
       </div>
     </div>
-  );
+  )
 }
