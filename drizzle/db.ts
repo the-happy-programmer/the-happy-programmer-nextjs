73a16c21583f1b23d43acdb2dc513da49bc68db1
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-07-12 20:56:51 +0100

    cc

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
 
 const sql = neon(process.env.DATABASE_URL!)
 export const db = drizzle(sql);
