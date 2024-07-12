09d89439e4a4e550a7d21997001a4f3b233b1041
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-07-12 18:31:22 +0100

    cc

--- auth.ts
+++ auth.ts
@@ -1,5 +1,15 @@
 import NextAuth from "next-auth"
- 
+import Github from "next-auth/providers/github"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import { db } from "./drizzle/db"
+import {accounts, sessions, users, verificationTokens } from "./drizzle/schema"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  providers: [Github],
 })
--- drizzle/db.ts
+++ drizzle/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 
-config({ path: ".env" });
+config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
