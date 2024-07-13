c33d704aef1501dc69d7860063a15f3ce60c12c7
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-07-13 19:41:08 +0100

    cc

--- app/(auth)/auth/error/page.tsx
+++ app/(auth)/auth/error/page.tsx
@@ -1,5 +1,35 @@
+import { RxExclamationTriangle } from 'react-icons/rx'
+import { Button } from '@nextui-org/button'
+import { Spotlight } from '@/components/ui/Spotlight'
 const page = ({ searchParams }: { searchParams: { error: string } }) => {
-  return <div>{JSON.stringify(searchParams)}</div>
+  console.log()
+  if (searchParams.error === 'Configuration') {
+    return (
+      <div className="">
+        <div className="h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+          <Spotlight
+            className="-top-40 left-0 md:left-60 md:-top-20"
+            fill="white"
+          />
+          <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+            <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Configuration <br />
+              Error
+            </h1>
+            <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+              We are sorry, but there was an error with the configuration of the
+              email. Please try again later.
+            </p>
+            <div className="mx-auto w-fit pt-10">
+              <Button>Go Back</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return <p>hello</p>
 }
 
 export default page
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-const {nextui} = require("@nextui-org/react");
-
+const { nextui } = require('@nextui-org/react')
 
 module.exports = {
   content: [
@@ -30,6 +29,21 @@ module.exports = {
       textOpacity: ['dark'],
       backgroundColor: ['checked'],
       borderColor: ['checked'],
+      animation: {
+        spotlight: 'spotlight 2s ease .75s 1 forwards',
+      },
+      keyframes: {
+        spotlight: {
+          '0%': {
+            opacity: 0,
+            transform: 'translate(-72%, -62%) scale(0.5)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translate(-50%,-40%) scale(1)',
+          },
+        },
+      },
     },
   },
   plugins: [
