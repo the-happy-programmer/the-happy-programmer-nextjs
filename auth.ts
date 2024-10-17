2a0e21007e7e09d4652665f9cd0cce0baa70af71
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2024-10-17 10:22:28 +0100

    cc

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
 import { db } from '@/drizzle/db'
 import { betterAuth } from 'better-auth'
-import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { drizzleAdapter } from 'better-auth/dist/adapters/drizzle'
+import {
+  bearer,
+  organization,
+  passkey,
+  twoFactor,
+  admin,
+} from 'better-auth/dist/plugins'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg' }),
